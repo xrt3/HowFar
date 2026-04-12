89c96b80db2849d45e88627a9e5c5c74855a9451
xrt3 <xrt3@outlook.com>
2026-04-12 23:08:13 +0800

    Initial Commit

---
+++ MileMate/MileMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  MileMateApp.swift
+//  MileMate
+//
+//  Created by BigTong on 2026/4/12.
+//
+
+import SwiftUI
+
+@main
+struct MileMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
